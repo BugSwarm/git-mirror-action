5823ccc3b38953ef086793759fc2ef3969f80fbf
Desmond Ding <info@desmonding.me>
2020-02-04 14:59:13 -0800

    Test

--- git-mirror.sh
+++ git-mirror.sh
@@ -11,11 +11,11 @@ echo "SOURCE=$SOURCE_REPO"
 echo "DESTINATION=$DESTINATION_REPO"
 
 echo "Cloning..."
-git clone --mirror "$SOURCE_REPO" && cd `basename "$SOURCE_REPO"`
+git clone --bare "$SOURCE_REPO" && cd `basename "$SOURCE_REPO"`
 git remote set-url --push origin "$DESTINATION_REPO"
 
 echo "Fetching..."
-git fetch -p origin +refs/heads/*:refs/heads/* +refs/tags/*:refs/tags/* +refs/change/*:refs/change/*
+git fetch -p origin
 
 echo "Pushing..."
 git push --mirror
