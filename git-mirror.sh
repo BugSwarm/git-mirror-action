9274b5ff5c46455f4b3a691fa7e68361ffdd9aff
Desmond Ding <info@desmonding.me>
2020-02-10 13:07:01 -0800

    Fix

--- git-mirror.sh
+++ git-mirror.sh
@@ -10,13 +10,12 @@ GIT_SSH_COMMAND="ssh -v"
 echo "SOURCE=$SOURCE_REPO"
 echo "DESTINATION=$DESTINATION_REPO"
 
-echo "Cloning SOURCE_REPO..."
 git clone $SOURCE_REPO
 
-cd `basename -s .git "$SOURCE_REPO"`
+REPO_NAME=`basename -s .git $SOURCE_REPO`
+echo "REPO_NAME=$REPO_NAME"
+cd `basename -s .git $SOURCE_REPO`
 
-echo "Setting remote origin to DESTINATION_REPO..."
 git remote set-url --push origin $DESTINATION_REPO
 
-echo "Pushing to master..."
 git push origin master
